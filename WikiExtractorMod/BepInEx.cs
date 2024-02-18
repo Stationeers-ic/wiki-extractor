6e987885392c700a8e146f4e24d7058f01400078
Kirill <traineratwot@yandex.ru>
2024-02-18 21:39:06 +0300

    Init

---
+++ WikiExtractorMod/BepInEx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace WikiExtractorMod
+{
+    #region BepInEx
+
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class ExtractorBepInEx : BaseUnityPlugin
+    {
+        public const string pluginGuid = "net.elmo.stationeers.Extractor";
+        public const string pluginName = "Extractor";
+        public const string pluginVersion = "1.1";
+
+        public static void Log(string line)
+        {
+            Debug.Log("[" + pluginName + "]: " + line);
+        }
+
+        private void Awake()
+        {
+            try
+            {
+                var harmony = new Harmony(pluginGuid);
+                harmony.PatchAll();
+                Log("Patch WikiExtractorMod succeeded");
+            }
+            catch (Exception e)
+            {
+                Log("Patch WikiExtractorMod Failed");
+                Log(e.ToString());
+            }
+        }
+    }
+
+    #endregion
+}
---
+++ WikiExtractorMod/Extractor.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using Assets.Scripts.UI;
+using HarmonyLib;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace WikiExtractorMod
+{
+    using System.Collections.Generic;
+
+    [HarmonyPatch(typeof(Stationpedia), nameof(Stationpedia.Register))]
+    public class Extractor
+    {
+        [HarmonyPrefix]
+        public static void Prefix(StationpediaPage page, bool fallback = false)
+        {
+            var obj = new Dictionary<string, dynamic>();
+            obj.Add("Key", page.Key);
+            obj.Add("Title", page.Title);
+
+            obj.Add("LogicInsert", page.LogicInsert);
+            obj.Add("LogicSlotInsert", page.LogicSlotInsert);
+            obj.Add("ModeInsert", page.ModeInsert);
+            obj.Add("ConnectionInsert", page.ConnectionInsert);
+            obj.Add("ResourcesUsed", page.ResourcesUsed);
+            obj.Add("BuildStates", page.BuildStates);
+
+            obj.Add("PrefabName", page.PrefabName);
+            obj.Add("PrefabHashString", page.PrefabHashString);
+            obj.Add("PrefabHash", page.PrefabHash);
+            string json = JsonConvert.SerializeObject(obj);
+            string path = Path.Combine(
+                Path.Combine(Application.dataPath, "wiki_data"),
+                Application.dataPath, page.Key + ".json"
+            );
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                writer.WriteLine(json);
+            }
+
+            ExtractorBepInEx.Log(path);
+        }
+    }
+}
